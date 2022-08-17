9be2450f2144f325465e001cb9fef5b64d9411d0
nap-owen <nap.owenmontemor@gmail.com>
2022-08-17 14:02:26 +0800

    made header in HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from "@react-navigation/native"
+import {
+    UserIcon,
+    ChevronDownIcon,
+    SearchIcon,
+    AdjustmentsIcon,
+} from "react-native-heroicons/outline"
 
 export default function HomeScreen() {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        })
+    }, []);
+
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white pt-5">
+      <Text className="border-solid border-2 border-sky-500">
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2 w-full pr-7">
+            <Image
+                source={{
+                    uri: 'https://links.papareact.com/wru'
+                }}
+                className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+            />
+            <View className="flex-1">
+                <Text className="fon-bold text-gray-400 text-xs">
+                    Deliver Now!</Text>
+                <Text className="font-bold text-xl">
+                    Current Location
+                    <ChevronDownIcon size={20} color="#00CCBB" />
+                </Text>
+            </View>
+
+            <UserIcon size={35} color="#00CCBB"/>
+        </View>
+      </Text>
+    </SafeAreaView>
   )
 }
